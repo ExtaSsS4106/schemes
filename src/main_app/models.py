b0d16b5954da8683306b08e3653cc06a288b4816
ExtaSsS4106 <ExtaZzZ123456789@yandex.ru>
2026-02-09 02:22:53 +0300

    feat: added model schemes

--- src/main_app/models.py
+++ src/main_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Schemes(models.Model):
+    title = models.CharField(max_length=255, null=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
+    height = models.IntegerField(null=True)
+    width = models.IntegerField(null=True)
+    components = models.JSONField(blank=True,default=list,null=True)
+    
+    def __str__(self):
+        return self.id, self.title
